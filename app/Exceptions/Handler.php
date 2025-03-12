c2e916269e3ca8ff7e6d4046b213e01cbd062915
jackwio <will582145@gmail.com>
2025-03-13 00:37:03 +0800

    錯誤時，返回錯誤訊息

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    // Laravel 預設會回傳 HTML 格式的錯誤頁面，而不是 JSON。可以強制 Laravel 在錯誤時回傳 JSON。
+    public function render($request, Throwable $exception): JsonResponse
+    {
+        return response()->json([
+            'error' => $exception->getMessage()
+        ], 500);
     }
 }
