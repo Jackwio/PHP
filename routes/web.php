507c0dbcfa00fd8ad5b8e5aba5eb5814bc2e8828
jackwio <will582145@gmail.com>
2025-03-15 20:57:54 +0800

    調整 API Route 位置

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CurrencyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('/currency/convert', [CurrencyController::class, 'exchange']);
